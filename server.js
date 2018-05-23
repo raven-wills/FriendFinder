74263f427316abe966628cba69ffed5be6207920
Raven Wills <raven.wills@gmail.com>
2018-05-23 11:20:31 -0500

    Added connection to heroku

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var fs = require("fs");
 var apiRoutes = require("app/routing/apiRoutes");
+var bodyParser = require("body-parser");
+var path = require("path");
 
 var app = express();
+var PORT = process.env.PORT || 3000;
 
-var PORT = 8080;
-
-app.listen();
-module.exports = Server;
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
