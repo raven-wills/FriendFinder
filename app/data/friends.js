7067dd503d3b538e1a168800186faf55849a6a48
Raven Wills <raven.wills@gmail.com>
2018-05-24 13:58:14 -0500

    Update materialize

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,6 +4,24 @@ var friendsData = [
     friendImage:
       "https://upload.wikimedia.org/wikipedia/commons/a/a7/Head_of_Raven.jpg",
     scores: [1, 2, 2, 3, 1, 2, 4, 5, 1, 2]
+  },
+  {
+    friendName: "Corgi",
+    friendImage:
+      "https://upload.wikimedia.org/wikipedia/commons/a/a7/Head_of_Raven.jpg",
+    scores: [1, 2, 2, 3, 1, 2, 4, 5, 1, 2]
+  },
+  {
+    friendName: "Goldfish",
+    friendImage:
+      "https://upload.wikimedia.org/wikipedia/commons/a/a7/Head_of_Raven.jpg",
+    scores: [1, 2, 2, 3, 1, 2, 4, 5, 1, 2]
+  },
+  {
+    friendName: "Snow Leopard",
+    friendImage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRxxrD510ymlFbFvh8rQty8_Vom9Sc3-JWwniJmv3pACTHomps",
+    scores: [1, 2, 2, 3, 1, 2, 4, 5, 1, 2]
   }
 ];
 
